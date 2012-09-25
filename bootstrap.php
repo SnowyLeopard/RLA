48f5bbd83fa36d2ceb02ef727234f0a923ca8e78
Michon van Dooren <michon1992@gmail.com>
2012-09-25 18:30:24 +0200

    Made this into a proper bootstrap. Root is determined dynamically, and it enables twig, propel en symfony2.

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,40 @@
 <?php
 
-// Include the main Propel script
-require_once 'propel/Propel.php';
+// Determine the root dir.
+$root = dirname(__FILE__);
 
-// Root rla directory.SRTHYUIKOP;[']
-$root = "/home/michon/Coding/Local/rla";
+/** 
+ * Propel, the database management system.
+ */
+require_once 'propel/Propel.php';
 
 // Initialize Propel with the runtime configuration
 Propel::init("$root/build/conf/rla-conf.php");
 
 // Add the generated 'classes' directory to the include path
 set_include_path("$root/build/classes" . PATH_SEPARATOR . get_include_path());
+
+/**
+ * Twig, the template engine.
+ */
+require_once 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem("$root/templates");
+$twig = new Twig_Environment($loader, array(
+  'cache' => "$root/.cache",
+  'debug' => true,
+));
+
+/**
+ * Symfony classloader.
+ */
+require_once 'Symfony/Component/ClassLoader/UniversalClassLoader.php';
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->useIncludePath(true);
+$loader->register();
 
 // Helper functions.
 function printr($array)
