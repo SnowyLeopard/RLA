3258530488d71ff69dba2289fc8ecf08e6c8acc6
Michon van Dooren <michon1992@gmail.com>
2012-09-26 22:21:07 +0200

    Added coments to the password functions in the User class, and added a permission check function.

--- build/classes/rla/User.php
+++ build/classes/rla/User.php
@@ -12,17 +12,35 @@
  * @package    propel.generator.rla
  */
 class User extends BaseUser 
-
+{
+  /** 
+   * Set the password for this user.
+   *
+   * You don't have to worry about hashing or salting, this will be done 
+   * auctomatically. Also, a new salt will be generated and stored each time a 
+   * new password is set.
+   */
   public function setPassword($passwd)
   {
     parent::setPassword(crypt($passwd, $this->generateSalt()));
   }
 
+  /**
+   * Check's whether the provided password matches.
+   *
+   * Since we use hashing and salting and all, simply comparing the password to 
+   * getPassword()'s return value won't do.
+   */
   public function checkPassword($passwd)
   {
     return crypt($passwd, $this->getSalt()) == $this->getPassword();
   }
 
+  /**
+   * Generate a new salt.
+   *
+   * The salt is generated in a way that the php crypt() function will use blowfish.
+   */
   public function generateSalt()
   {
     $salt = '$2y$15$' . sha1(uniqid(rand(), TRUE)) . '$';
@@ -30,4 +48,20 @@ class User extends BaseUser
     return $salt;
   }
 
+  /**
+   * Permission levels.
+   */
+  static $LEVELS = array(
+    'admin' => 100,
+    'mod'   => 50,
+    'user'  => 5,
+  );
+
+  /**
+   * Check whether the user's level is high enough.
+   */
+  public function hasPermission($level)
+  {
+    return self::$LEVELS[$this->getLevel()] >= self::$LEVELS[$level];
+  }
 } // User
